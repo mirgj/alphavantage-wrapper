5dcc8cbec5f6255ac46b2d4a377a09b137aee192
mirgj <mirash.gjolaj@gmail.com>
2018-10-21 23:34:29 +0800

    Combine url to use into any specific category

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import crypto from './categories/crypto';
 
 export default config => {
   const parsedConfig = parser(config);
+  parsedConfig.url = `${parsedConfig.baseUrl}?apikey=${parsedConfig.apiKey}&`;
 
   return {
     crypto: crypto(parsedConfig),
