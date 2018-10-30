99635495babbbfb60a9959849bfbd721f6074c0c
mirgj <mirash.gjolaj@gmail.com>
2018-10-30 23:04:59 +0800

    Add in digital currency daily

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 export const CURRENCY_EXCHANGE_RATE = 'CURRENCY_EXCHANGE_RATE';
+export const DIGITAL_CURRENCY_DAILY = 'DIGITAL_CURRENCY_DAILY';
 export const DYNAMIC = '[dynamic]';
 export const DYNAMIC_FORMAT = '[dynamicFormat]';
