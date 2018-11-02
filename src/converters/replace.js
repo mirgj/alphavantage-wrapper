292968ba74517e0b07bbd1e2c4002b1dc65a5f36
mirgj <mirash.gjolaj@gmail.com>
2018-11-03 00:25:08 +0800

    add in test for replacer

--- src/converters/replace.js
+++ src/converters/replace.js
@@ -1,8 +1,8 @@
 export default (value, context) => {
-  if (!value) throw new Error(`'The provided value is not valid ${value}`);
-  if (!context) throw new Error(`'context is required`);
-  if (!context.from) throw new Error(`'context.from is required`);
-  if (!context.to) throw new Error(`'context.to is required`);
+  if (!value) throw new Error(`The provided value is not valid: ${value}`);
+  if (!context) throw new Error(`context is required`);
+  if (!context.from) throw new Error(`context.from is required`);
+  if (!context.to) throw new Error(`context.to is required`);
 
-  return value.replace(context.from, context.to);
+  return value.split(context.from).join(context.to);
 };
---
+++ src/converters/replace.spec.js
@@ -0,0 +1,58 @@
+import replacer from './replace';
+
+describe('# replace', () => {
+  it('should replace the value correctly', () => {
+    const string = 'a-string-a';
+    const context = {
+      from: 'a',
+      to: 'b',
+    };
+    const result = replacer(string, context);
+
+    expect(result).toEqual('b-string-b');
+  });
+
+  it.each([null, undefined, ''])(
+    'should throw an exception if the value is %s',
+    key => {
+      expect(() => replacer(key)).toThrowError(
+        `The provided value is not valid: ${key}`,
+      );
+    },
+  );
+
+  it.each([null, undefined, ''])(
+    'should throw an exception if the context is %s',
+    key => {
+      expect(() => replacer('good-value', key)).toThrowError(
+        `context is required`,
+      );
+    },
+  );
+
+  it.each([null, undefined, ''])(
+    'should throw an exception if the context.from is %s',
+    key => {
+      const context = {
+        from: key,
+        to: 'to',
+      };
+      expect(() => replacer('good-value', context)).toThrowError(
+        `context.from is required`,
+      );
+    },
+  );
+
+  it.each([null, undefined, ''])(
+    'should throw an exception if the context.to is %s',
+    key => {
+      const context = {
+        from: 'from',
+        to: key,
+      };
+      expect(() => replacer('good-value', context)).toThrowError(
+        `context.to is required`,
+      );
+    },
+  );
+});
