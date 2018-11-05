3ac890e87e1795ae187de4ddb1f63f8b7188d4f1
mirgj <mirash.gjolaj@gmail.com>
2018-11-06 00:30:02 +0800

    Update type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,11 +2,38 @@ interface IConfig {
   apiKey: string;
   baseUrl: string;
   injectRawResponse: boolean;
+  convert: boolean;
+  validate: boolean;
+  parse: string;
 }
 
 interface ICurrency {
   code: string;
   name: string;
+}
+
+interface IMarketValue {
+  open: string | number;
+  high: string | number;
+  low: string | number;
+  close: string | number;
+}
+
+interface ICyptoMarketValue extends IMarketValue {
+  marketCap: string | number;
+}
+
+interface ICryptoTimeType {
+  [key: string]: ICyptoMarketValue;
+  volume: string | number;
+}
+
+interface ICyptoTimeSeries {
+  [key: string | Date]: ICryptoTimeType;
+}
+
+interface IForexTimeSeries {
+  [key: string | Date]: IMarketValue;
 }
 
 interface IExchangeRateResult {
@@ -17,8 +44,55 @@ interface IExchangeRateResult {
   timeZone: string;
 }
 
-interface ICrypto {}
-interface IForex {}
+interface ICryptoResult {
+  description: string;
+  currency: ICurrency;
+  exchange: ICurrency;
+  lastRefresh: string | Date;
+  timeZone: string;
+  timeSeries: ICyptoTimeSeries;
+}
+
+interface IForexResult {
+  description: string;
+  from: ICurrency;
+  to: ICurrency;
+  outputType: string;
+  lastRefresh: string | Date;
+  timeZone: string;
+  timeSeries: IForexTimeSeries;
+}
+
+interface ICrypto {
+  daily(symbol: string, market: string): ICryptoResult;
+  weekly(symbol: string, market: string): ICryptoResult;
+  monthly(symbol: string, market: string): ICryptoResult;
+}
+interface IForex {
+  intraday(
+    from_currency: string,
+    to_currency: string,
+    interval?: string,
+    outputSize?: string,
+    dataType?: string,
+  ): IForexResult;
+  daily(
+    from_currency: string,
+    to_currency: string,
+    outputSize?: string,
+    dataType?: string,
+  ): IForexResult;
+  weekly(
+    from_currency: string,
+    to_currency: string,
+    dataType?: string,
+  ): IForexResult;
+  monthly(
+    from_currency: string,
+    to_currency: string,
+    dataType?: string,
+  ): IForexResult;
+}
 
 interface IResult {
   crypto: ICrypto;
